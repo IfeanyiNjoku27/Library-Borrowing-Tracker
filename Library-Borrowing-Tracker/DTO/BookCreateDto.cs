00a4f2968d0cab558c0daaa2f9eba87f5b727907
Olamilekan Johnson <ojohns32@my.centennialcollege.ca>
2026-08-11 18:40:30 -0400

    Commit to feature/books-controller

---
+++ Library-Borrowing-Tracker/DTO/BookCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Borrowing_Tracker.DTO
+{
+    public class BookCreateDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public bool isAvailable { get; set; } = true;
+    }
+}
---
+++ Library-Borrowing-Tracker/DTO/BookReadDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Library_Borrowing_Tracker.DTO
+{
+    public class BookReadDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public bool isAvailable { get; set; }
+    }
+}
